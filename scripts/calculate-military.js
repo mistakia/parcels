f92161455fede915d184ecc1e3e5162eb47211e0
mistakia <1823355+mistakia@users.noreply.github.com>
2023-02-10 22:19:13 -0500

    refactor: tweak

--- scripts/calculate-military.js
+++ scripts/calculate-military.js
@@ -79,7 +79,7 @@ const calculate_military_for_parcels = async (parcels) => {
       ...data
     })
 
-    if (inserts.length >= 10) {
+    if (inserts.length >= 100) {
       await save_military(inserts)
       inserts = []
     }
--- scripts/calculate-spring.js
+++ scripts/calculate-spring.js
@@ -83,7 +83,7 @@ const calculate_spring_for_parcels = async (parcels) => {
       ...data
     })
 
-    if (inserts.length >= 10) {
+    if (inserts.length >= 100) {
       await save_spring(inserts)
       inserts = []
     }
