f783e152d470b03dd56490667ccb54d15c84bdb1
mistakia <1823355+mistakia@users.noreply.github.com>
2023-05-03 20:22:45 -0400

    feat: calculate ranks for `parcels_weather` columns

--- scripts/calculate-ranks.mjs
+++ scripts/calculate-ranks.mjs
@@ -12,6 +12,9 @@ debug.enable('calculate-ranks')
 
 const calculate_rank_for_field = async ({ field, table_name, order_by }) => {
   console.time(field)
+  await db.raw(
+    `ALTER TABLE parcels_rank ADD COLUMN IF NOT EXISTS ${field}_rank smallint`
+  )
   await db.raw(`
     INSERT INTO parcels_rank (ll_uuid, ${field}_rank)
     SELECT pa.ll_uuid, ((1 - (PERCENT_RANK() OVER (ORDER BY pa.${field} ${order_by}))) * 100)
@@ -110,6 +113,44 @@ const calculate_ranks = async () => {
       order_by: 'DESC'
     }
   ]
+
+  const parcels_weather_columns = await db('information_schema.columns')
+    .select('column_name')
+    .where('table_name', 'parcels_weather')
+    .whereNotIn('column_name', ['ll_uuid', 'updated'])
+
+  const ascending_weather_types = [
+    'hrs_below_0_c_apparent_temperature',
+    'hrs_below_5_c_apparent_temperature',
+    'hrs_below_10_c_apparent_temperature',
+    'daytime_hrs_below_0_c_apparent_temperature',
+    'daytime_hrs_below_5_c_apparent_temperature',
+    'daytime_hrs_below_10_c_apparent_temperature',
+    'indoor_days',
+    'daytime_hours_above_10_pct_cloud_cover',
+    'daytime_hours_above_20_pct_cloud_cover',
+    'daytime_hours_above_30_pct_cloud_cover',
+    'daytime_hours_above_40_pct_cloud_cover',
+    'daytime_hours_above_50_pct_cloud_cover',
+    'daytime_hours_above_60_pct_cloud_cover',
+    'daytime_hours_above_70_pct_cloud_cover',
+    'daytime_hours_above_80_pct_cloud_cover',
+    'daytime_hours_above_90_pct_cloud_cover',
+    'daytime_hours_above_100_pct_cloud_cover'
+  ]
+  for (const weather_column of parcels_weather_columns) {
+    const { column_name } = weather_column
+    if (column_name.includes('_dates')) {
+      continue
+    }
+    const weather_column_type = column_name.replace(/_in_[0-9]{4}/gi, '')
+    const is_asc = ascending_weather_types.includes(weather_column_type)
+    items.push({
+      field: column_name,
+      table_name: 'parcels_weather',
+      order_by: is_asc ? 'ASC' : 'DESC'
+    })
+  }
 
   for (const item of items) {
     log(`calculating rank for ${item.field}`)
